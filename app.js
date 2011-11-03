8021a92f8fb1c6d1ba99b44ae72fd5b8d491cdb6
ssherman <shane.sherman@gmail.com>
2011-11-03 16:28:48 -0500

    fix weird issue with creating directory on every request munging the stream

--- app.js
+++ app.js
@@ -49,56 +49,54 @@ app.use(connect.router(function (app) {
             console.log(s3_res.headers);
 
             // create a directory with the same name as the db on the autonomy server
-            fs.mkdir(dir_to_save_docs_to + db_name, 0755, function (e) {
-                var path_to_file = dir_to_save_docs_to + db_name + "/" + filename;
+            var path_to_file = dir_to_save_docs_to + db_name + "_" + filename;
 
-                // stream the document to disk chunk by chunk
-                var outstream = fs.createWriteStream(path_to_file);
-                s3_res.on('data', function (chunk) {
-                    outstream.write(chunk);
+            // stream the document to disk chunk by chunk
+            var outstream = fs.createWriteStream(path_to_file);
+            s3_res.on('data', function (chunk) {
+                outstream.write(chunk);
+            });
+
+            // the file has been saved! now let's build the autonomy request
+            s3_res.on('end', function () {
+                console.log("Submitting the data to autonomy filesystemfetch");
+                outstream.end();
+                var xml = "<?xml version=\"1.0\"?><autn:import><autn:envelope><autn:stubidx><![CDATA[" + stubidx + "]]></autn:stubidx><autn:document><autn:fetch url=\"" + path_to_file + "\" deleteoriginal=\"true\" /></autn:document></autn:envelope></autn:import>";
+                console.log(xml);
+
+                var idol_data = querystring.stringify({
+                    'Data': stubidx,
+                    'DREDBNAME': db_name,
+                    'EnvelopeXML': xml,
+                    'jobname': 'ImportEnvelopeJob',
+                    'EnvelopeImportFailOnImport': 'never'
                 });
 
-                // the file has been saved! now let's build the autonomy request
-                s3_res.on('end', function () {
-                    console.log("Submitting the data to autonomy filesystemfetch");
-                    outstream.end();
-                    var xml = "<?xml version=\"1.0\"?><autn:import><autn:envelope><autn:stubidx><![CDATA[" + stubidx + "]]></autn:stubidx><autn:document><autn:fetch url=\"" + path_to_file + "\" deleteoriginal=\"true\" /></autn:document></autn:envelope></autn:import>";
-                    console.log(xml);
+                var http_options = {
+                    host: 'localhost',
+                    port: 7000,
+                    path: '/action=ImportEnvelope',
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Content-Length': idol_data.length
+                    }
+                };
 
-                    var idol_data = querystring.stringify({
-                        'Data': stubidx,
-                        'DREDBNAME': db_name,
-                        'EnvelopeXML': xml,
-                        'jobname': 'ImportEnvelopeJob',
-                        'EnvelopeImportFailOnImport': 'never'
+                // post to autonomy to index the document
+                var post_req = http.request(http_options, function (res) {
+                    res.setEncoding('utf8');
+                    res.on('data', function (chunk) {
+                        console.log('Response: ' + chunk);
                     });
-
-                    var http_options = {
-                        host: 'localhost',
-                        port: 7000,
-                        path: '/action=ImportEnvelope',
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/x-www-form-urlencoded',
-                            'Content-Length': idol_data.length
-                        }
-                    };
-
-                    // post to autonomy to index the document
-                    var post_req = http.request(http_options, function (res) {
-                        res.setEncoding('utf8');
-                        res.on('data', function (chunk) {
-                            console.log('Response: ' + chunk);
-                        });
-                        res.on('error', function (e) {
-                            console.log('problem with request: ' + e.message);
-                        });
+                    res.on('error', function (e) {
+                        console.log('problem with request: ' + e.message);
                     });
-
-                    // post the data
-                    post_req.write(idol_data);
-                    post_req.end();
                 });
+
+                // post the data
+                post_req.write(idol_data);
+                post_req.end();
             });
         }).end();
         res.writeHead(200, {
@@ -115,10 +113,10 @@ app.use(connect.router(function (app) {
         };
 
         // post to autonomy to index the document
-        var unindex_request = http.get(http_options, function(res) {
-          console.log("unindex response: " + res.statusCode);
-        }).on('error', function(e) {
-          console.log("unindex error: " + e.message);
+        var unindex_request = http.get(http_options, function (res) {
+            console.log("unindex response: " + res.statusCode);
+        }).on('error', function (e) {
+            console.log("unindex error: " + e.message);
         });
 
         res.writeHead(200, {
